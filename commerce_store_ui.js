bb7fdb01298e8eeb22e3165a02c109437b657059
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2013-04-05 11:56:27 -0400

    Store edit form - added user field, regrouped fields, vertical tabs.

---
+++ commerce_store_ui.js
@@ -0,0 +1,38 @@
+
+(function ($) {
+
+Drupal.behaviors.commerceStoreUIFieldsetSummaries = {
+  attach: function (context) {
+    $('fieldset.store-form-revision-information', context).drupalSetSummary(function (context) {
+      var revisionCheckbox = $('.form-item-revision input', context);
+
+      // Return 'New revision' if the 'Create new revision' checkbox is checked,
+      // or if the checkbox doesn't exist or is not checked, but some text was
+      // added to the revision log.
+      if (revisionCheckbox.is(':checked') || $('.form-item-log textarea', context).val().length) {
+        return Drupal.t('New revision');
+      }
+
+      return Drupal.t('No revision');
+    });
+
+    $('fieldset.store-form-owner', context).drupalSetSummary(function (context) {
+      return Drupal.t('@name', { '@name': $('.form-item-name input', context).val() });
+    });
+
+    $('fieldset.store-form-options', context).drupalSetSummary(function (context) {
+      var vals = [];
+
+      $('input:checked', context).parent().each(function () {
+        vals.push(Drupal.checkPlain($.trim($(this).text())));
+      });
+
+      if (!$('.form-item-status input', context).is(':checked')) {
+        vals.unshift(Drupal.t('Disabled'));
+      }
+      return vals.join(', ');
+    });
+  }
+};
+
+})(jQuery);
